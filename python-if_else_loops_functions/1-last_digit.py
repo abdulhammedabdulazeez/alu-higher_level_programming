cfa759e8492572ecf425c19085cdebf558772580
abdulhammedabdulazeez <a.abdulazee@alustudent.com>
2023-02-10 01:15:53 -0800

    New commit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -13,4 +13,4 @@ if (last_digit > 5):
 elif (last_digit == 0):
     print(f"and is 0")
 elif((last_digit < 6) and (last_digit != 0)):
-    print(f"and is less than 6 and not 0") 
+    print(f"and is less than 6 and not 0")
