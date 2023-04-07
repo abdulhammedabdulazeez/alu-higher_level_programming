d82430171acae45f64ad71262919a124ab9cdce4
root <a.abdulazee@alustudent.com>
2023-04-07 14:27:15 +0000

    New Commit

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
 """I documented you"""
 
-from urllib.request import urlopen, Request
-import urllib.parse
+from urllib.request import urlopen
 import sys
 
 if __name__ == '__main__':
-    """"Documented"""
-    url = sys.argv[1]
-    values = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(values)
-    data = data.encode('ascii')
-    req = Request(url, data)
-    with urlopen(req) as res:
-        content = res.read()
-        print("{}".format(content.decode("utf-8")))
+    with urlopen(sys.argv[1]) as res:
+        header = res.info()
+        print(header["X-Request-Id"])
