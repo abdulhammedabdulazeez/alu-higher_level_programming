169e33e90a67847e5bcb4625ff131ffe40b3c590
unknown <a.abdulazee@alustudent.com>
2023-06-16 18:11:03 +0200

    new file

--- javascript-warm_up/2-arguments.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const { argv } = process;
-
-const argvLen = argv.length;
-
-if (argvLen < 3) console.log('No argument');
-if (argvLen === 3) console.log('Argument found');
-if (argvLen > 3) console.log('Arguments found');
